8c0ec7860750a8b2720eda1f291a9e85c36bbeea
janemiumiu <80030066+janemiumiu@users.noreply.github.com>
2022-01-02 23:49:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,65 +9,9 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 from fsm import TocMachine
 from utils import send_text_message
-
+from machine import create_machine
 load_dotenv()
-
-
-machine = TocMachine(
-    states=["initial", "search_album_by_song", "intro_album","indtro_album_other_song","recommand_song","recommand_song_by_album","random_collect_song","about_jay"],
-    transitions=[
-        {
-            "trigger": "advance",
-            "source": "initial",
-            "dest": "search_album_by_song",
-            "conditions": "is_going_to_search_album_by_song",
-        },
-        {
-            "trigger": "advance",
-            "source": "search_album_by_song",
-            "dest": "intro_album",
-            "conditions": "is_going_to_intro_album",
-        },
-        {
-            "trigger": "advance",
-            "source": "search_album_by_song",
-            "dest": "indtro_album_other_song",
-            "conditions": "is_going_to_indtro_album_other_song",
-        },
-        {
-            "trigger": "advance",
-            "source": "initial",
-            "dest": "recommand_song",
-            "conditions": "is_going_to_recommand_song",
-        },
-        {
-            "trigger": "advance",
-            "source": "recommand_song",
-            "dest": "recommand_song_by_album",
-            "conditions": "is_going_to_recommand_song_by_album",
-        },
-        {
-            "trigger": "advance",
-            "source": "recommand_song",
-            "dest": "random_collect_song",
-            "conditions": "is_going_to_random_collect_song",
-        },
-        {
-            "trigger": "advance",
-            "source": "initial",
-            "dest": "about_jay",
-            "conditions": "is_going_to_about_jay",
-        },
-        {"trigger": "advance",
-         "source": ["search_album_by_song", "intro_album","indtro_album_other_song","recommand_song","recommand_song_by_album","random_collect_song","about_jay"], 
-        "dest": "initial",
-        "conditions":"is_going_to_initial"
-        }
-    ],
-    initial="initial",
-    auto_transitions=False,
-    show_conditions=True,
-)
+machines={}
 
 app = Flask(__name__, static_url_path="")
 
@@ -134,26 +78,26 @@ def webhook_handler():
             continue
         if not isinstance(event.message.text, str):
             continue
-        print(f"\nFSM STATE: {machine.state}")
-        print(f"REQUEST BODY: \n{body}")
-        response = machine.advance(event)
+        if event.source.user_id not in machines:
+            machines[event.source.user_id] = create_machine()
+        response = machines[event.source.user_id].advance(event)
         if response == False:
-            if machine.state=="initial":
-                send_text_message(event.reply_token, "請輸入正確的關鍵字！歌名 或是 “推薦歌曲” 或是 ”關於周杰倫“")
+            if machines[event.source.user_id].state=="initial":
+                send_text_message(event.reply_token, "請輸入正確的關鍵字！歌名 或是 “推薦的歌” 或是 ”關於周杰倫“")
             
-            if machine.state=="search_album_by_song":
+            if machines[event.source.user_id].state=="search_album_by_song":
                 send_text_message(event.reply_token, "請輸入正確的關鍵字！“介紹專輯” 或是 ”專輯裡的其他首歌“")
 
-            if machine.state=="intro_album":
+            if machines[event.source.user_id].state=="intro_album":
                 send_text_message(event.reply_token, "查無專輯！")
 
-            if machine.state=="indtro_album_other_song":
+            if machines[event.source.user_id].state=="indtro_album_other_song":
                 send_text_message(event.reply_token, "查無專輯！")
             
-            if machine.state=="recommand_song":
+            if machines[event.source.user_id].state=="recommand_song":
                 send_text_message(event.reply_token, "請輸入正確的關鍵字！專輯名稱 或是 “隨機“")
             
-            if machine.state=="recommand_song_by_album":
+            if machines[event.source.user_id].state=="recommand_song_by_album":
                 send_text_message(event.reply_token, "請輸入正確的專輯名稱！")
                 
             
@@ -161,12 +105,13 @@ def webhook_handler():
     return "OK"
 
 
-@app.route("/show-fsm", methods=["GET"])
+'''@app.route("/show-fsm", methods=["GET"])
 def show_fsm():
     machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
+    return send_file("fsm.png", mimetype="image/png")'''
 
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 8000)
     app.run(host="0.0.0.0", port=port, debug=True)
+
